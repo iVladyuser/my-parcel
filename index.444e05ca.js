3ff379d9f2d4e2b34299cb45df28e06913cabd30
iVladyuser <129949946+iVladyuser@users.noreply.github.com>
2023-08-13 18:01:18 +0000

    Deploying to gh-pages from @ iVladyuser/my-parcel@b2f4081c36148f4779dca0cb7bd8f5067e230d1c 🚀

---
+++ index.444e05ca.js
@@ -0,0 +1,2 @@
+!function(){var e,t=document.querySelector("#alertButton"),n=document.querySelector("#alertInput");t.addEventListener("click",(e=n,function(t){e.value.trim()||console.log("привіт")}));var o,r,c=(o=10,r=20,function(){return o+r+30});console.log(c())}();
+//# sourceMappingURL=index.444e05ca.js.map
---
+++ index.746d2d73.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#alertButton"),t=document.querySelector("#alertInput");var n;e.addEventListener("click",(n=t,function(e){n.value.trim()||console.log("привіт")}));const o=(c=10,r=20,function(){return c+r+30});var c,r;console.log(o());
+//# sourceMappingURL=index.746d2d73.js.map
